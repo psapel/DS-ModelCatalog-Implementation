ae610a652e16d9eb4b0b570d100690601617d509
Anna-grf <75178572+Anna-grf@users.noreply.github.com>
2023-03-09 11:32:27 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 from dotenv import load_dotenv
 load_dotenv()
+
+import meilisearch
+from meilisearch import Client
+
+import json
+import os
 
 app = Flask(__name__)
 
@@ -11,8 +17,9 @@ app = Flask(__name__)
 
 def checkbox():
     if request.method == 'POST':
-        print(request.form.getlist('checked'))
-        return 'OK'
+        x = request.form.getlist('checked')
+        print(x)
+        return 'ok'
     return render_template('index.html')
 
 if __name__ == '__main__':
@@ -20,29 +27,135 @@ if __name__ == '__main__':
 
 #RADIO BUTTONS
 
-@app.route('/radio_button', methods=['POST'])
+# Path to directory containing JSON files
+json_dir = 'C:/Users/annga/OneDrive/Desktop/HiWi - IKV/GUI/gui/DS-ModelCatalog/json_files/'
+scripts_dir = 'C:/Users/annga/OneDrive/Desktop/HiWi - IKV/GUI/gui/DS-ModelCatalog/scripts/'
+client = meilisearch.Client('http://127.0.0.1:7700', 'masterKey')
+index = client.index('json_files')
+index.update_filterable_attributes(['gender', 'nationality'])
+results = []
+for file in os.listdir(json_dir):
+        if file.endswith('.json'):
+            with open(os.path.join(json_dir, file), 'r') as f:
+                data = json.load(f)
+                results.append(data)
+index.add_documents(results)
 
-@app.route('/radio_button')
+
+@app.route('/radio_button', methods=['GET', 'POST'])
 def radio_button():
+ 
     ans = request.form['gender']
-    if ans == "Male" or ans== "Female" or ans=="Non-binary":
-        return f"{ans} student"
+    gender = index.search(ans)
+                  
+    if ans == 'Non-binary' or ans == 'other':
+        return f"No module found"
+    elif ans == 'Male':
+        output = ''
+        for a in gender['hits']:
+            output = output + 'module' + " " + str(a['id']) + '<br>'
+        return output
     else:
-        return f"Student of {ans} gender"
-
-  
-if __name__ == '__main__':
+        output1 = ''
+        for b in gender['hits']:
+            if b['nationality']=='non-EU':
+                output1 = output1 + 'module' + " " + str(b['id']) + '<br>'
+        return output1
+         
+ 
+if __name__=='__main__':
     app.run(debug=True, use_reloader=True)
-
+        
 #DROPDOWN MENU
 
 @app.route('/')
 @app.route("/dropdown" , methods=['GET', 'POST'])
 def dropdown():
-    ans_1 = request.form.get('choice')
+    ans_2 = request.form.get('choice')
     #return(str(ans_1))
-    return f"I like % s" % ans_1
+    return f"I like % s" % ans_2
 
 if __name__=='__main__':
     app.run(debug=True, use_reloader=True)
+    
+    
+#RADIO BUTTONS - new!   
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    
+    ans = request.form['gender']
+    gender = index.search(ans)
+                        
+    if ans == 'Non-binary' or ans == 'other':
+        return f"No module found"
+    elif ans == 'Male':
+        output = ''
+        for a in gender['hits']:
+            output = output + 'module' + " " + str(a['id']) + '<br>'
+        return output
+    else:
+        return redirect('/nationality')
+
+
+
+@app.route('/nationality', methods=['GET', 'POST'])
+def nationality():
+    if request.method == 'POST':
+        ans1 = request.form.get('nationality')
+        nasF = index.search('',
+            {
+                'filter': f'gender=Female AND nationality={ans1}'
+            }
+        )
+        
+           
+        if ans1=='EU':
+            output1 = ''
+            for b in nasF['hits']:
+                output1 = output1 + 'Module' + " " + str(b['id']) + '<br>'
+            return redirect(url_for('convert', out=output1))  
+             
+        elif ans1=='Non-EU':
+            output2 = ''
+            for c in nasF['hits']:
+                output2 = output2 + 'Module' + " " + str(c['id']) + '<br>'
+            return redirect(url_for('convert', out=output2))
+         
+          
+    return render_template('nationality.html')
+
+
+@app.route('/convert/<out>', methods=['GET','POST'])
+
+# convert output of the nationality function into a string 
+def convert(out):
+    if request.method=='GET':
+        out  = out[:-4] 
+        out = out.split("<br>")
+        out = ','.join(out)
+
+        return redirect(url_for('selection', out=out))
+
+             
+             
+#CHECKBOXES - new!
+@app.route('/selection/<out>', methods=['GET','POST'])
+
+def selection(out):
+     # Convert the input string into a list of strings
+    out_list = out.split(",")
+    
+    if request.method == 'POST':
+        # Get the selected module value from the form submission
+        selected_module = request.form.getlist('module')
+        selected_module = '<br>'.join(' '.join(item.split())  for item in selected_module)
+
+        return selected_module
+    else:
+        # Render the selection.html template with the out_list variable
+        return render_template('selection.html', out_list=out_list)
+    
+if __name__ == '__main__':
+    app.run(debug=True) 
 
